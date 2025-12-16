7abbbec819bfab371234ca3c6cb3b27da07e4c9d
aoi0412 <aoiaoi0412.1203@gmail.com>
2025-12-16 17:06:15 +0900

    update comment & likedList field at each types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,14 +1,18 @@
 export interface User {
-    id: string;
-    name: string;
-    totalPages: number;
-    updatedAt: Date;
+  id: string;
+  name: string;
+  totalPages: number;
+  comment: string;
+  likedList: string[];
+  updatedAt: Date;
 }
 
 export interface ReadingLog {
-    id: string;
-    userId: string;
-    userName: string;
-    pages: number;
-    createdAt: Date;
+  id: string;
+  userId: string;
+  userName: string;
+  pages: number;
+  comment: string;
+  likedList: string[];
+  createdAt: Date;
 }
